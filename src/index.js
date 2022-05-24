5cffb313f46090c1f89925fd92feb20fc4671d39
Nester44 <nesterov.mail4@gmail.com>
2022-05-24 14:36:49 +0200

    added colission to platforms

--- src/index.js
+++ src/index.js
@@ -74,6 +74,19 @@ document.addEventListener('DOMContentLoaded', () => {
       if (doodlerBottomSpace <= 0) {
         gameOver();
       }
+      platforms.forEach((platform) => {
+        if (
+          (doodlerBottomSpace >= platform.bottom) &&
+              (doodlerBottomSpace <= platform.bottom + platform.bottom + 15) &&
+              ((doodlerLeftSpace + 60) >= platform.left) &&
+              (doodlerLeftSpace <= (platform.left + 85)) &&
+              !isJumping
+        ) {
+          console.log('landed');
+          startPoint = doodlerBottomSpace;
+          jump();
+        }
+      });
     }, 30);
   }
 
