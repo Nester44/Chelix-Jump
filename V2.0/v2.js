cb71f27adb5608d3c56ebbacb07f0aabf8828227
Nester44 <nesterov.mail4@gmail.com>
2022-06-19 13:23:09 +0200

    Directory managment for version 2.0

--- V2.0/v2.js
+++ V2.0/v2.js
@@ -3,6 +3,8 @@
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const startBtn = document.querySelector('.start-btn');
+
 
 class Vector {
   constructor(x, y) {
@@ -11,6 +13,9 @@ class Vector {
   }
   dup() {
     return new Vector(this.x, this.y);
+  }
+  add(v) {
+    return new Vector(this.x + v.x, this.y + v.y);
   }
 }
 
@@ -46,12 +51,16 @@ class Doodler {
     this.isJump = false;
     this.isFall = false;
 
-    this.acc_y = 1;
+    this.acc_y = 0.5;
     this.vel_y = 0;
 
     this.vel_x = 0;
     this.acc_x = 0;
     this.acceleration = 1;
+    this.jumpHeight = 15;
+
+    this.animID;
+
   }
   drawDood() {
     ctx.beginPath();
@@ -66,11 +75,13 @@ class Doodler {
 class Game {
   constructor() {
     this.platGap = 100;
-    this.platSpeed = 0.5;
+    this.platSpeed = 1;
     this.platforms = [];
+    this.moveHeight = 350;
     this.createPlat();
 
     const firstPlat = this.platforms[0].position.dup();
+    // get center of plat
     firstPlat.x += this.platforms[0].width / 2;
     this.dood = new Doodler(firstPlat);
 
@@ -80,13 +91,14 @@ class Game {
   }
 
   createPlat() {
-    for (let i = 1; i < 7; i++) {
+    for (let i = 1; i < 7; i++) { // set border canvas.height / this.platGap
       const plat = new Platform(this.platGap * i);
       this.platforms.push(plat);
     }
   }
 
   movePlat() {
+    this.dood.isAbove = this.dood.position.y < this.moveHeight;
     if (!this.dood.isAbove) return;
     this.platforms.forEach((p) => {
       p.position.y += this.platSpeed;
@@ -101,13 +113,11 @@ class Game {
   }
 
   detColl() {
-    console.log('detColl invoked');
     const doodRight = this.dood.position.x + this.dood.width;
     const doodBottom = this.dood.position.y + this.dood.height;
 
     for (let i = 0; i < this.platforms.length; i++) {
       const p = this.platforms[i];
-      console.log(p);
       const platRight = p.position.x + p.width;
       const platBottom = p.position.y + p.height;
       if (
@@ -115,9 +125,8 @@ class Game {
         this.dood.position.x <= platRight &&
         doodBottom <= platBottom &&
         doodBottom >= p.position.y &&
-        this.dood.vel_y >= 3
+        this.dood.vel_y >= 3 // magic needed falling speed
       ) {
-        console.log('colission');
         this.dood.isJump = true;
         return true;
       } else {
@@ -125,6 +134,37 @@ class Game {
       }
     }
     return false;
+  }
+
+  move() {
+    // X moving
+    if (this.dood.left) this.dood.acc_x = -this.dood.acceleration;
+    if (this.dood.right) this.dood.acc_x = this.dood.acceleration;
+
+    if (!this.dood.right && !this.dood.left) this.dood.acc_x = 0;
+
+    this.dood.vel_x += this.dood.acc_x;
+    this.dood.vel_x *= 1 - this.friction;
+    // bouncing function
+    if (this.detColl()) {
+      this.dood.vel_y = -this.dood.jumpHeight;
+    } else {
+      this.dood.vel_y += this.dood.acc_y;
+    }
+
+    this.dood.position.x += this.dood.vel_x;
+    this.dood.position.y += this.dood.vel_y;
+
+    // teleportation between edges
+    if (this.dood.position.x + this.dood.width <= 0) {
+      this.dood.position.x = ctx.canvas.width - this.dood.width / 2;
+    }
+    if (this.dood.position.x >= ctx.canvas.width) {
+      this.dood.position.x = -this.dood.width / 2;
+    }
+
+    // gameOver check
+    if (this.dood.position.y >= ctx.canvas.height) this.gameOver();
   }
 
   keyControl() {
@@ -137,48 +177,30 @@ class Game {
       if (e.code === 'KeyA') this.dood.left = false;
       if (e.code === 'KeyD') this.dood.right = false;
     });
-    // X moving
-    if (this.dood.left) this.dood.acc_x = -this.dood.acceleration;
-    if (this.dood.right) this.dood.acc_x = this.dood.acceleration;
-
-    if (!this.dood.right && !this.dood.left) this.dood.acc_x = 0;
-
-    this.dood.vel_x += this.dood.acc_x;
-    this.dood.vel_x *= 1 - this.friction;
-
-    this.dood.position.x += this.dood.vel_x;
-    // bouncing function
-    if (this.detColl()) {
-      this.dood.vel_y = -15; // magic jmp height
-    } else {
-      this.dood.vel_y += 0.5; // magic Y speed
-    }
-    this.dood.position.y += this.dood.vel_y;
-
-    // teleportation between edges
-    if (this.dood.position.x + this.dood.width <= 0) {
-      this.dood.position.x = ctx.canvas.width - this.dood.width / 2;
-    }
-    if (this.dood.position.x >= ctx.canvas.width) {
-      this.dood.position.x = -this.dood.width / 2;
-    }
   }
 
   mainLoop() {
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     this.platforms.forEach((p) => {
       p.drawPlat();
-
       this.movePlat();
     });
     this.keyControl();
+    this.move();
     this.dood.drawDood();
-    requestAnimationFrame(this.mainLoop.bind(this));
+    this.animID = requestAnimationFrame(this.mainLoop.bind(this));
   }
   start() {
-    requestAnimationFrame(this.mainLoop.bind(this));
+    this.animID = requestAnimationFrame(this.mainLoop.bind(this));
+  }
+  gameOver() {
+    cancelAnimationFrame(this.animID);
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
   }
 }
 
-const game = new Game();
-game.start();
+startBtn.addEventListener('click', () => {
+  const game = new Game();
+  game.start();
+});
+
