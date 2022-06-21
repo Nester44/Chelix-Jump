44aa5b9b5224c99f41af776b2eb4e11039c642c0
Nester44 <nesterov.mail4@gmail.com>
2022-06-21 20:37:18 +0200

    Change project name

--- V2.0/v2.js
+++ V2.0/v2.js
@@ -292,7 +292,7 @@ class Game {
     const loop = () => {
       ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
       ctx.font = '3vw Arial';
-      const txt = 'Doodle Jump';
+      const txt = 'Chelix Jump';
       const x = canvas.clientWidth / 2 - (ctx.measureText(txt).width / 2);
       const y = canvas.clientHeight * 0.175;
       ctx.fillText(txt, x, y);
